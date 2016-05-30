44965e8740e9d5cd2d14b65b765d997d9ac4db1c
Tea <tea.desouza@gmail.com>
2016-05-30 15:11:50 +0800

    Output a cute thing to the terminal/console

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std; int main(){
+
+   cout << "Hello Kitty" << endl;
+   cout << "           " << endl;
+   cout << "  > O O <  " << endl;
+   cout << "           " << endl;
+   cout << "  THE END  " << endl;
+   return 0;
+
+}
