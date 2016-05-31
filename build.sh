083baf7aff72719a50ee6c8e4bae67c3c166535b
Tea <tea.desouza@gmail.com>
2016-05-31 20:31:11 +0800

    Remove unsightly conditional clause

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-if [ $# -eq 0 ]; then
-  echo "Build with Jenkins"
-elif [ "$1" = "local" ]; then
-  echo "Build locally"
-  rm -R build
-else
-  echo "Invalid argument '$1'; script will exit."
-  exit 1
-fi
 
+# Convenience script to build and run.
+# NOTE - not used by the Jenkins build worker.
+
+rm -R build
 mkdir build
 cd build
 cmake ..
